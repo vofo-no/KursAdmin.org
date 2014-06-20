7e537a3b330023bc5a519ce69fb685376e340c9b
Mats Grimsgaard <mats@mgv.no>
2014-06-20 15:31:20 +0200

    js minify + updates

--- js/site.js
+++ js/site.js
@@ -1,18 +1,18 @@
-$(function(){
-    var fadeBegin = 10, 
-    fadeFinish = 400, 
-    fadingElement = $('div.scroll-down');
+/*!
+ * jQuery Cookie Plugin v1.4.1
+ * https://github.com/carhartl/jquery-cookie
+ *
+ * Copyright 2013 Klaus Hartl
+ * Released under the MIT license
+ */
+!function(e){"function"==typeof define&&define.amd?define(["jquery"],e):e("object"==typeof exports?require("jquery"):jQuery)}(function(e){function n(e){return u.raw?e:encodeURIComponent(e)}function o(e){return u.raw?e:decodeURIComponent(e)}function i(e){return n(u.json?JSON.stringify(e):String(e))}function r(e){0===e.indexOf('"')&&(e=e.slice(1,-1).replace(/\\"/g,'"').replace(/\\\\/g,"\\"));try{return e=decodeURIComponent(e.replace(c," ")),u.json?JSON.parse(e):e}catch(n){}}function t(n,o){var i=u.raw?n:r(n);return e.isFunction(o)?o(i):i}var c=/\+/g,u=e.cookie=function(r,c,f){if(void 0!==c&&!e.isFunction(c)){if(f=e.extend({},u.defaults,f),"number"==typeof f.expires){var a=f.expires,d=f.expires=new Date;d.setTime(+d+864e5*a)}return document.cookie=[n(r),"=",i(c),f.expires?"; expires="+f.expires.toUTCString():"",f.path?"; path="+f.path:"",f.domain?"; domain="+f.domain:"",f.secure?"; secure":""].join("")}for(var p=r?void 0:{},s=document.cookie?document.cookie.split("; "):[],m=0,x=s.length;x>m;m++){var v=s[m].split("="),k=o(v.shift()),l=v.join("=");if(r&&r===k){p=t(l,c);break}r||void 0===(l=t(l))||(p[k]=l)}return p};u.defaults={},e.removeCookie=function(n,o){return void 0===e.cookie(n)?!1:(e.cookie(n,"",e.extend({},o,{expires:-1})),!e.cookie(n))}});
 
-$(window).on('scroll', function(){
-    var offset = $(document).scrollTop(), opacity = 0; 
-    console.log(offset);
-    if( offset <= fadeBegin ){
-        opacity = 1; 
-    } else if( offset <= fadeFinish ){
-        opacity = 1 - offset / fadeFinish;
 
-    }
-    fadingElement.css('opacity',opacity);
-});
-    
-    });
+/* ------- site.js ------- */
+
+/* FADE-on-scroll-function: o = fadeBegin, c=fadeFinish */
+$(function(){var o=10,c=400,n=$("div.scroll-down");$(window).on("scroll",function(){var l=$(document).scrollTop(),s=0;console.log(l),o>=l?s=1:c>=l&&(s=1-l/c),n.css("opacity",s)})});
+
+/* COOKIE-remember-selected-option: e = select id / cookie name */
+$(function(){var e="stf-valg";null!=$.cookie(e)&&$("#"+e+' option[value="'+$.cookie(e)+'"]').attr("selected","selected"),$("#"+e).change(function(){$.cookie(e,$("#"+e+" option:selected").val(),{expires:90,path:"/"})})});
+
