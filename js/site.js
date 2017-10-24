0b6783d8a2794fb977545f06cb1991b2009cc467
Mats Grimsgaard <mats@mgv.no>
2017-10-24 12:21:02 +0200

    Update site.js

--- js/site.js
+++ js/site.js
@@ -6,9 +6,9 @@ if (typeof(Storage) !== "undefined") {
   $(document).on("change", "#stf-valg", function() {
     var v = $(this).val();
     localStorage.setItem("valgt-stf", v);
-    $("#stf-table tr").removeClass("active");
-    $("#banner-form-login").attr("href", "https://www.kursadmin.org/pls/kas/f?p=" + v).attr("disabled", !!!v);
-    $("#banner-form-login-test").attr("href", "http://test.senitel.no/pls/kastest/f?p=" + v).attr("disabled", !!!v);
+    $("#stf-table tr").removeClass("active");    
+    $("#banner-form-login").attr("href", v && "https://www.kursadmin.org/pls/kas/f?p=" + v).attr("disabled", !!!v);
+    $("#banner-form-login-test").attr("href", v && "http://test.senitel.no/pls/kastest/f?p=" + v).attr("disabled", !!!v);
     $("#stf-row-" + v).addClass("active");
   });
   $(function() {
